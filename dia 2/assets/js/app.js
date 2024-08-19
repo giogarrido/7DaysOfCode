28352bd3fcc6e0574a218e06317240f164531e80
GARRIDO\giova <giovanni.garrido228724@potros.itson.edu.mx>
2024-08-19 15:59:55 -0700

    dia 2 extra

--- dia 2/assets/js/app.js
+++ dia 2/assets/js/app.js
@@ -2,6 +2,8 @@ let nameUser ="";
 let age = 0;
 let programmingLanguage = "";
 let message = "";
+let message2 = "";
+let option = 0;
 
 
 nameUser = prompt("¿Cuál es tu nombre?");
@@ -14,5 +16,20 @@ message = `Hola ${nameUser}, tienes ${age} años y ya estás aprendiendo ${progr
 // despliega un mensaje como alerta
 alert(message);
 
+option = prompt(`¿Te gusta estudiar ${programmingLanguage}? Responde con el número 1 para Sí o 2 para No`);
+
+if(option == 1){
+    message2 = "¡Muy bien! Sigue estudiando y tendrás mucho éxito!";
+}
+else if(option == 2){
+    message2 = "Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?";
+}
+else{
+    message2 = "Opción no válida";
+}
+
 // despliega un mensaje en el documento
 document.getElementById("message").innerHTML = message;
+
+// despliega un mensaje en el documento
+document.getElementById("message2").innerHTML = message2;
